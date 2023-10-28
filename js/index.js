37475178625f697cd3afec5d400575af1b6d0c51
AaldertKroes <a.a.kroes@st.hanze.nl>
2023-10-28 20:04:28 +0200

    login page works
    If you're not logged in on the home page, you're redirected to the login page.
    If you login with u: henk | pw: detank you can now login (no database connection yet)
    CSS for login better.

--- js/index.js
+++ js/index.js
@@ -1,4 +1,9 @@
 console.log("index.js - start")
+
+if(localStorage.getItem("currentUser") === null) window.location.replace("./html/login.html");
+
+document.getElementById("welcome-name").innerHTML = `Welcome ${localStorage.getItem("currentUser")}`;
+
 document.getElementById("test-button").addEventListener('click', ()=>{
     document.getElementById("dash").innerHTML = "hey";
 });
--- js/login.js
+++ js/login.js
@@ -1,1 +1,18 @@
 console.log("login.js - start")
+
+let submitButton = document.getElementById("login");
+let username = "";
+let password = "";
+
+submitButton.addEventListener('click', evt => {
+    evt.preventDefault();
+    username = document.getElementById("username").value;
+    password = document.getElementById("password").value;
+
+    (username === "henk" && password === "detank" ? loginHandler() : window.location.replace("./login.html"));
+});
+
+function loginHandler(){
+    localStorage.setItem("currentUser", username);
+    window.location.href = "../index.html";
+}
